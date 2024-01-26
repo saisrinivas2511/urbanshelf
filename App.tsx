26e9daf48589c3f1b1decd9b980a92e6c60f40a9
Sai Srinivas <saisrinivas2511@gmail.com>
2024-01-26 15:31:25 +0530

    removed log boxes for better user experience

--- App.tsx
+++ App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {BottomNavigator} from './src/Navigation/BottomNavigator';
 import {Provider} from 'react-redux';
 import store from './src/Redux/store';
+import { LogBox } from 'react-native';
 
 const App = () => {
+  LogBox.ignoreAllLogs();
   return (
     <Provider store={store}>
       <BottomNavigator />
